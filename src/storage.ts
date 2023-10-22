787c4c8dd85190acb7786592d16313a9f1bc679a
Alexey Nebolsin <alexey@nebols.in>
2023-10-22 13:20:21 +0400

    Use session storage

--- src/storage.ts
+++ src/storage.ts
@@ -1,11 +1,11 @@
 export function getUserId(): string | null {
-  return window.localStorage.getItem("userId");
+  return window.sessionStorage.getItem("userId");
 }
 
 export function setUserId(userId: string) {
-  window.localStorage.setItem("userId", userId);
+  window.sessionStorage.setItem("userId", userId);
 }
 
 export function removeUserId() {
-  window.localStorage.removeItem("userId");
+  window.sessionStorage.removeItem("userId");
 }
