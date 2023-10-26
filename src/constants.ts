83bf25b3bf667b5c12fac72cddb425ab572f3d1d
Alexey Nebolsin <alexey@nebols.in>
2023-10-26 17:18:03 +0400

    Use local server when in development

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,11 @@
-export const ServerUrl =
-  "https://api.paybeaver.xyz";
+export let ServerUrl: string;
+if (
+  !process.env.NODE_ENV ||
+  process.env.NODE_ENV === "development"
+) {
+  ServerUrl = "http://127.0.0.1:8000";
+} else {
+  ServerUrl = "https://api.paybeaver.xyz";
+}
 
 export const MyMerchantDomain = "paybeaver.xyz";
